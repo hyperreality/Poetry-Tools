fc4828b1d5f42488643c5f280c2974bea401edb7
hyperreality <l.tennant@rocketmail.com>
2016-03-11 22:00:20 +0000

    update

--- poetrytools/tests/test_poetics.py
+++ poetrytools/tests/test_poetics.py
@@ -1,27 +1,34 @@
 from unittest import TestCase
 
-import os
-import poetrytools
-
-def open_poem(poem):
-    with open(os.path.join('poetrytools/poems',poem)) as f:
-        return f.read()
+import os, poetrytools
 
 class TestPoems(TestCase):
+    def open_poem(self, poem):
+        with open(os.path.join('poetrytools/poems', poem)) as f:
+            return f.read()
 
     def test_haiku(self):
-        self.assertTrue(poetrytools.guess_form(open_poem('haiku.txt')) == 'haiku')
-        self.assertTrue(poetrytools.guess_form(open_poem('tanka.txt')) == 'tanka')
+        self.assertTrue(poetrytools.guess_form(self.open_poem('haiku.txt')) == 'haiku')
+        self.assertTrue(poetrytools.guess_form(self.open_poem('tanka.txt')) == 'tanka')
 
     def test_sonnets(self):
-        self.assertTrue(poetrytools.guess_form(open_poem('sonnet.txt')) == 'Shakespearean sonnet')
-        self.assertTrue(poetrytools.guess_form(open_poem('brokensonnet.txt')) == 'sonnet with trochaic pentameter or irregular meter')
+        self.assertTrue(poetrytools.guess_form(self.open_poem('sonnet.txt')) == 'Shakespearean sonnet')
+        self.assertTrue(poetrytools.guess_form(self.open_poem('brokensonnet.txt')) == 'sonnet with trochaic pentameter or irregular meter')
 
     def test_blankverse(self):
-        self.assertTrue(poetrytools.guess_form(open_poem('blankverse.txt')) == 'blank verse')
+        self.assertTrue(poetrytools.guess_form(self.open_poem('blankverse.txt')) == 'blank verse')
 
     def test_cinquain(self):
-        self.assertTrue(poetrytools.guess_form(open_poem('cinquain.txt')) == 'cinquain')
+        self.assertTrue(poetrytools.guess_form(self.open_poem('cinquain.txt')) == 'cinquain')
+
+    def test_ottavarima(self):
+        self.assertTrue(poetrytools.guess_form(self.open_poem('ottavarima.txt')) == 'ottava rima')
+
+    def test_heroiccouplets(self):
+        self.assertTrue(poetrytools.guess_form(self.open_poem('heroiccouplets.txt')) == 'heroic couplets')
+
+    def test_rondeau(self):
+        self.assertTrue(poetrytools.guess_form(self.open_poem('rondeau.txt')) == 'rondeau')
 
 if __name__ == '__main__':
     unittest.main()
