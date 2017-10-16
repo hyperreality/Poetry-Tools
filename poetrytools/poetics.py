a54f29a5db7a3971cb25804351876e0dbc9a23b7
hyperreality <lmt@fastmail.com>
2017-10-16 20:32:56 +0100

    Rhyme function changes level for short words; style cleanup

--- poetrytools/poetics.py
+++ poetrytools/poetics.py
@@ -2,121 +2,148 @@
 #-*- coding: utf-8 -*-
 
 from __future__ import print_function
-import json, os, sys, codecs
+import json
+import os
+import sys
+import codecs
 from collections import defaultdict
 from string import ascii_lowercase
 from Levenshtein import distance
 from .countsyl import count_syllables
 from .simpletokenizer import tokenize
 
-# Import CMU dictionary
+# Load up the CMU dictionary
 with open(os.path.join(os.path.dirname(__file__), 'cmudict/cmudict.json')) as json_file:
-    cmu = json.load(json_file)
+    CMU = json.load(json_file)
 
-possible_metres = {
-    'iambic trimeter'     : '010101',
-    'iambic tetrameter'   : '01010101',
-    'iambic pentameter'   : '0101010101',
-    'trochaic tetrameter' : '10101010',
-    'trochaic pentameter' : '1010101010'
+POSSIBLE_METRES = {
+    'iambic trimeter': '010101',
+    'iambic tetrameter': '01010101',
+    'iambic pentameter': '0101010101',
+    'trochaic tetrameter': '10101010',
+    'trochaic pentameter': '1010101010'
 }
 
-possible_rhymes = {
-    'couplets'            : 'aabbccddeeff',
-    'alternate rhyme'     : 'ababcdcdefefghgh',
-    'enclosed rhyme'      : 'abbacddceffe',
-    'rima'                : 'ababcbcdcdedefefgfghg',
-    'rondeau rhyme'       : 'aabbaaabCaabbaC',
+POSSIBLE_RHYMES = {
+    'couplets': 'aabbccddeeff',
+    'alternate rhyme': 'ababcdcdefefghgh',
+    'enclosed rhyme': 'abbacddceffe',
+    'rima': 'ababcbcdcdedefefgfghg',
+    'rondeau rhyme': 'aabbaaabCaabbaC',
     'shakespearean sonnet': 'ababcdcdefefgg',
-    'limerick'            : 'aabba',
-    'no rhyme'            : 'XXXX'
+    'limerick': 'aabba',
+    'no rhyme': 'XXXX'
 }
 
-possible_stanzas = {
-    'sonnet'              : '14,',
-    'tercets'             : '3,'
+POSSIBLE_STANZAS = {
+    'sonnet': '14,',
+    'tercets': '3,'
 }
 
-def getSyllables(word):
+
+def get_syllables(word):
     """
     Look up a word in the CMU dictionary, return a list of syllables
     """
 
     try:
-        return(cmu[word.lower()])
+        return CMU[word.lower()]
     except KeyError:
         return False
+
 
 def stress(word):
     """
     Represent strong and weak stress of a word with a series of 1's and 0's
     """
 
-    syllables = getSyllables(word)
+    syllables = get_syllables(word)
 
     if syllables:
         # TODO: Implement a more advanced way of handling multiple pronunciations than just using the min
-        min_syllables = min(syllables)
-        pronunciation_string = str(''.join(min_syllables))
-        stress_numbers       = ''.join([x.replace('2', '1') for x in pronunciation_string if x.isdigit()]) # not interested in secondary stress
+        pronunciation_string = str(''.join(min(syllables)))
+        # Not interested in secondary stress
+        stress_numbers = ''.join([x.replace('2', '1')
+                                  for x in pronunciation_string if x.isdigit()])
 
         return stress_numbers
-    else:
-        # Provisional logic for adding stress when the word is not in the dictionary is to stress first syllable only
-        return '1' + '0' * (count_syllables(word) - 1) 
 
-def scanscion(tokenizedPoem):
+    # Provisional logic for adding stress when the word is not in the dictionary is to stress first syllable only
+    return '1' + '0' * (count_syllables(word) - 1)
+
+
+def scanscion(tokenized_poem):
     """
     Get stress notation for every line in the poem
     """
-    
-    line_stresses = []
-    currline      = 0
 
-    for line in tokenizedPoem:
+    line_stresses = []
+    currline = 0
+
+    for line in tokenized_poem:
         line_stresses.append([])
         [line_stresses[currline].append(stress(word)) for word in line if word]
         currline += 1
 
     return line_stresses
 
+
+def num_vowels(syllables):
+    return len([syl for syl in syllables if any(char.isdigit() for char in syl)])
+
+
 def rhymes(word1, word2, level=2):
     """
     For each word, get a list of various syllabic pronunications. Then check whether the last level number of syllables is pronounced the same. If so, the words probably rhyme
     """
 
-    pronunciations = getSyllables(word1)
-    pronunciations2 = getSyllables(word2)
+    pronunciations = get_syllables(word1)
+    pronunciations2 = get_syllables(word2)
 
-    if pronunciations and pronunciations2:
-        for syllable in pronunciations:
-            for syllable2 in pronunciations2:
-                if syllable2[-level:] == syllable[-level:]:
-                    return True
+    if not (pronunciations and pronunciations2):
+        return False
+
+    equivalents = {"ER0": "R"}
+    def replace_syllables(syllables):
+        return [equivalents[syl] if syl in equivalents else syl for syl in syllables]
+
+    for syllables in pronunciations:
+        syllables = replace_syllables(syllables)
+        syls = level # Default number of syllables to check back from
+        # If word only has a single vowel (i.e. 'stew'), then we reduce this to 1 otherwise we won't find a monosyllabic rhyme
+        if num_vowels(syllables) == 1:
+            syls = 1
+
+        for syllables2 in pronunciations2:
+            syllables2 = replace_syllables(syllables2)
+            if syllables[-syls:] == syllables2[-syls:]:
+                return True
+
     return False
 
-def rhyme_scheme(tokenizedPoem):
+
+def rhyme_scheme(tokenized_poem):
     """
     Get a rhyme scheme for the poem. For each line, lookahead to the future lines of the poem and see whether last words rhyme.
     """
 
     # By default, nothing rhymes
-    scheme = ['X'] * len(tokenizedPoem)
+    scheme = ['X'] * len(tokenized_poem)
 
     rhyme_notation = list(ascii_lowercase)
 
     currrhyme = 0
 
-    for lineno in range(0, len(tokenizedPoem)):
-        for futurelineno in range(lineno + 1, len(tokenizedPoem)):
-            # if next line is not already part of a rhyme scheme
+    for lineno in range(0, len(tokenized_poem)):
+        for futurelineno in range(lineno + 1, len(tokenized_poem)):
+            # If next line is not already part of a rhyme scheme
             if scheme[futurelineno] == 'X':
-                if tokenizedPoem[lineno] == ['']:
+                if tokenized_poem[lineno] == ['']:
                     # If blank line, represent that in the notation
                     scheme[lineno] = ' '
-                elif rhymes(tokenizedPoem[lineno][-1], tokenizedPoem[futurelineno][-1]):
+                elif rhymes(tokenized_poem[lineno][-1], tokenized_poem[futurelineno][-1]):
                     # Capitalise rhyme if the whole line is identical
-                    if tokenizedPoem[lineno] == tokenizedPoem[futurelineno]:
+                    if tokenized_poem[lineno] == tokenized_poem[futurelineno]:
                         scheme[lineno] = scheme[futurelineno] = rhyme_notation[currrhyme].upper()
                     else:
                         scheme[lineno] = scheme[futurelineno] = rhyme_notation[currrhyme]
@@ -124,12 +151,36 @@ def rhyme_scheme(tokenizedPoem):
 
     return scheme
 
+
+def stanza_lengths(tokenized_poem):
+    """
+    Returns a comma-delimited string of stanza lengths
+    """
+
+    stanzas = []
+
+    i = 0
+    for line in tokenized_poem:
+        if line != ['']:
+            i += 1
+        else:
+            stanzas.append(str(i))
+            i = 0
+    if i != 0:
+        stanzas.append(str(i))
+
+    joined = ','.join(stanzas)
+
+    return joined
+
+
 def get_lowest(dictionary):
     """
     Get the lowest value of a dictionary, returning its key
     """
 
-    return min(dictionary, key = dictionary.get)
+    return min(dictionary, key=dictionary.get)
+
 
 def levenshtein(string, candidates):
     """
@@ -146,67 +197,56 @@ def levenshtein(string, candidates):
 
     return get_lowest(distances)
 
-def guess_metre(tokenizedPoem):
+
+def guess_metre(tokenized_poem):
     """
     Guess a poem's metre via Levenshtein distance from candidates
     """
 
-    joined_lines = [''.join(line) for line in scanscion(tokenizedPoem) if line]
+    joined_lines = [''.join(line) for line in scanscion(tokenized_poem) if line]
     line_lengths = [len(line) for line in joined_lines]
-    num_lines    = len(joined_lines)
+    num_lines = len(joined_lines)
 
     z = defaultdict(int)
     for line in joined_lines:
-        for metre, notation in possible_metres.items():
+        for metre, notation in POSSIBLE_METRES.items():
             z[metre] += distance(line, notation)
 
     guessed_metre = get_lowest(z)
 
     return joined_lines, num_lines, line_lengths, guessed_metre
 
-def guess_rhyme_type(tokenizedPoem):
+
+def guess_rhyme_type(tokenized_poem):
     """
     Guess a poem's rhyme via Levenshtein distance from candidates
     """
 
-    joined_lines = ''.join([l for l in rhyme_scheme(tokenizedPoem) if l])
+    joined_lines = ''.join([l for l in rhyme_scheme(tokenized_poem) if l])
     no_blanks = joined_lines.replace(' ', '')
 
-    guessed_rhyme = levenshtein(no_blanks, possible_rhymes)
+    guessed_rhyme = levenshtein(no_blanks, POSSIBLE_RHYMES)
     return joined_lines, guessed_rhyme
 
-def stanza_lengths(tokenizedPoem):
-    """
-    Returns a comma-delimited string of stanza lengths
-    """
 
-    stanzas = []
+def guess_stanza_type(tokenized_poem):
+    joined_lengths = stanza_lengths(tokenized_poem)
 
-    i = 0
-    for line in tokenizedPoem:
-        if line != ['']:
-            i += 1
-        else:
-            stanzas.append(str(i))
-            i = 0
-    if i != 0:
-        stanzas.append(str(i))
+    guessed_stanza = levenshtein(joined_lengths, POSSIBLE_STANZAS)
+    return joined_lengths, guessed_stanza
 
-    joined = ','.join(stanzas)
 
-    guessed_stanza = levenshtein(joined, possible_stanzas)
-    return joined, guessed_stanza
-
-def guess_form(tokenizedPoem, verbose=False):
+def guess_form(tokenized_poem, verbose=False):
     def within_ranges(line_properties, ranges):
         if all([ranges[i][0] <= line_properties[i] <= ranges[i][1] for i in range(len(ranges))]):
             return True
 
-    metrical_scheme, num_lines, line_lengths, metre = guess_metre(tokenizedPoem)
-    rhyme_scheme_string, rhyme = guess_rhyme_type(tokenizedPoem)
-    stanza_length_string, stanza = stanza_lengths(tokenizedPoem)
+    metrical_scheme, num_lines, line_lengths, metre = guess_metre(
+        tokenized_poem)
+    rhyme_scheme_string, rhyme = guess_rhyme_type(tokenized_poem)
+    stanza_length_string, stanza = guess_stanza_type(tokenized_poem)
 
-    if verbose == True:
+    if verbose:
         print("Metre: " + ' '.join(metrical_scheme))
         print("Rhyme scheme: " + rhyme_scheme_string)
         print("Stanza lengths: " + stanza_length_string)
@@ -214,7 +254,7 @@ def guess_form(tokenizedPoem, verbose=False):
         print("Closest metre: " + metre)
         print("Closest rhyme: " + rhyme)
         print("Closest stanza type: " + stanza)
-        print("Guessed form: ",end="")
+        print("Guessed form: ", end="")
 
     if num_lines == 3 and within_ranges(line_lengths, [(4, 6), (6, 8), (4, 6)]):
         return 'haiku'
@@ -242,7 +282,6 @@ def guess_form(tokenizedPoem, verbose=False):
         return 'sonnet with ' + metre + ' or irregular meter'
 
     if num_lines == 15:
-
         return 'rondeau'
 
     if rhyme == 'alternate rhyme' and metre == 'iambic tetrameter':
@@ -254,7 +293,8 @@ def guess_form(tokenizedPoem, verbose=False):
     if metre == 'iambic pentameter':
         return 'blank verse'
 
-    return 'unknown form' 
+    return 'unknown form'
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
